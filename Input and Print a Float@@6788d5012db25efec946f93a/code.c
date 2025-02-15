5a4e76509fcffbfe0da29c3e6e65e7eae8123d87
JainishaJain-09 <jainisha1100.becse24@chitkara.edu.in>
2025-02-15 17:30:06 +0530

    Solution to Input and Print a Float

--- Input and Print a Float@@6788d5012db25efec946f93a/code.c
+++ Input and Print a Float@@6788d5012db25efec946f93a/code.c
@@ -3,7 +3,7 @@ int main(){
     float a = 3.14;
     float b = 0.0;
     scanf("%f %f",&a,&b);
-    printf("You entered%f",a);
-    printf("You entered%f",b);
+    printf("You entered: %f\n",a);
+    printf("You entered: %f\n",b);
     return 0;
 }
